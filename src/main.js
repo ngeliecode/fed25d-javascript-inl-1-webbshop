5d865be9b5518e5c083175613c6576f1e203126c
Angelie <angelie.angman@gmail.com>
2026-01-14 19:29:58 +0100

    update js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,62 @@
 import "./styles/style.scss";
 
+//====================================================
+//========= NAVIGATION ===============================
+//====================================================
+
+const navBtn = document.querySelector("#hamburger");
+const navMenu = document.querySelector(".navbar");
+
+navBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  navMenu.classList.toggle("active");
+} // const toggleMenu = function() {...};
+
+//====================================================
+//========= PRODUCTS =================================
+//====================================================
+
 // const = deklaration
-// product = variabelnamn
+// products = variabelnamn
+// [] = array (lista)
 // {} = objekt (property = key + value)
 
-const product = {
-  name: "Produkt 1",
-  price: 15,
-};
+//====================================================
 
-const product2 = {
-  name: "Produkt 2",
-  price: 15,
-};
+// 1. Skapa en produktlista (array med objekt)
+const products = [
+  {
+    // objektets egenskaper
+    name: "Jordgubbsmunk", // key (string) + value (string)
+    price: 15, // key (string) + value (number)
+  },
+  {
+    name: "Chokladmunk",
+    price: 10,
+  },
+  {
+    name: "Vaniljmunk",
+    price: 19,
+  },
+  {
+    name: "Sockermunk",
+    price: 15,
+  },
+];
+
+console.log(products);
+
+// 2. Välj vilka element jag vill koppla till arrayen
+const productName = document.querySelectorAll(".product-name");
+const productPrice = document.querySelectorAll(".product-price");
+
+console.log(productName, productPrice);
+
+// 3. Koppla varje objekt till rätt element men en loop
+for (let i = 0; i < products.length; i++) {
+  productName[i].textContent = products[i].name;
+  productPrice[i].textContent = products[i].price + " kr";
+}
+
+// 4. Nu kan jag se produkterna i webbläsaren
