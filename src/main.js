0126d8015190b7ff91a854db571a64ddb87e562d
Angelie <angelie.angman@gmail.com>
2026-01-14 14:00:02 +0100

    update js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import "./styles/style.scss";
+
+// const = deklaration
+// product = variabelnamn
+// {} = objekt (property = key + value)
+
+const product = {
+  name: "Produkt 1",
+  price: 15,
+};
+
+const product2 = {
+  name: "Produkt 2",
+  price: 15,
+};
