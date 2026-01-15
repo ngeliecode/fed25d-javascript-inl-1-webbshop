e6730caa32f48437ada5422b31390d07cd494a6e
Angelie <angelie.angman@gmail.com>
2026-01-15 13:21:20 +0100

    update js

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ function toggleMenu() {
 //========= PRODUCTS =================================
 //====================================================
 
-// const = deklaration
+// const = deklarerar en variabel
 // products = variabelnamn
 // [] = array (lista)
 // {} = objekt (property = key + value)
@@ -28,19 +28,43 @@ function toggleMenu() {
 const products = [
   {
     // objektets egenskaper
-    name: "Jordgubbsmunk", // key (string) + value (string)
+    name: "Produktnamn 1", // key (string) + value (string)
     price: 15, // key (string) + value (number)
   },
   {
-    name: "Chokladmunk",
+    name: "Produktnamn 2",
     price: 10,
   },
   {
-    name: "Vaniljmunk",
+    name: "Produktnamn 3",
     price: 19,
   },
   {
-    name: "Sockermunk",
+    name: "Produktnamn 4",
+    price: 15,
+  },
+  {
+    name: "Produktnamn 5",
+    price: 15,
+  },
+  {
+    name: "Produktnamn 6",
+    price: 15,
+  },
+  {
+    name: "Produktnamn 7",
+    price: 15,
+  },
+  {
+    name: "Produktnamn 8",
+    price: 15,
+  },
+  {
+    name: "Produktnamn 9",
+    price: 15,
+  },
+  {
+    name: "Produktnamn 10",
     price: 15,
   },
 ];
@@ -51,12 +75,12 @@ console.log(products);
 const productName = document.querySelectorAll(".product-name");
 const productPrice = document.querySelectorAll(".product-price");
 
-console.log(productName, productPrice);
-
 // 3. Koppla varje objekt till rätt element men en loop
 for (let i = 0; i < products.length; i++) {
   productName[i].textContent = products[i].name;
   productPrice[i].textContent = products[i].price + " kr";
 }
-
-// 4. Nu kan jag se produkterna i webbläsaren
+// for = loop
+// let = deklarerar en variabel
+// i = variabelnamn
+// 0 = startvärde
