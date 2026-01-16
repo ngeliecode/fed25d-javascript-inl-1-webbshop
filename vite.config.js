9ca0133c929103ad05764d1b2b2efa67f5686db9
Angelie <angelie.angman@gmail.com>
2026-01-16 12:00:12 +0100

    change path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/ngeliecode/fed25d-javascript-inl-1-webbshop/",
+  base: "/fed25d-javascript-inl-1-webbshop/",
 });
 
 // base: "" = anger rätt sökväg vid publicering på GitHub Pages
-// ex: "/användarnamn/repo-namn/"
